420a1af2b8c5933d776c325a99eae1d8fa86b141
Imane Saci <imene2931@outlook.fr>
2025-10-21 16:17:47 +0200

    Ajout dossier src

---
+++ src/train_model.py
@@ -0,0 +1,7 @@
+# Script d'entraînement du modèle 
+
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
